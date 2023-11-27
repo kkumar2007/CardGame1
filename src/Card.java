91a0d598c5e241c5713a96470a3b14034f14ff90
Kavan <kavan.kumar@menloschool.org>
2023-11-27 09:49:27 -0800

    Added an instance variable to Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    private String suit;
+    private int value;
+    public Card()
+    {
+        suit = "Diamonds";
+        value = 6;
+    }
 }
